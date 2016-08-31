e510a87af0c9d8a9400aa3e76344050063a68e5d
eml11 <eml11@ic.ac.uk>
2016-08-31 18:58:47 +0100

    fixed allocate added sum

--- rungekutta.f90
+++ rungekutta.f90
@@ -2,24 +2,29 @@ subroutine rungekutta(func,y,x,order)
   !explicit
   interface
     function func(y,x)
-      double precision, allocatable y
+      double precision, dimension(:) y
       double precision x
     end function
   end interface
 
-  double precision, allocatable y
-  double precision, size(2) x
+  double precision, dimension(:) y
+  double precision, dimension(2) x
   integer order !not sure if there is an efficient formula for this
   
   double precision, allocatable retar
   double precision, allocatable k
   double precision deltax
   
+  allocate(retar(y.size))
+  allocate(k(y.size))
+  
   deltax = (x(2) - x(1))/100.0
+  
+  retar = y
   
   do 
     k = func(y,x(1))
-    retar = retar + k
+    retar = retar + k/6.0D0
   
     k = func(y + k, x(1) + 0.5D0*deltax)
     retar = retar + 2D0*k
@@ -31,17 +36,22 @@ subroutine rungekutta(func,y,x,order)
     retar = retar + k
     retar = retar/6.0D0
     x(1) = x(1) + deltax
+    y = retar
 
+    ! may need to go at top
     if x(1).ge.x(2)
       break
     end if
 
   end do
 
+  deallocate(retar)
+  deallocate(k)
+
 end subroutine
 
 function simple_decay(y,x)
-  double precision, allocatable y
+  double precision, dimension(:) y
   double precision x
 
   return -x
@@ -49,7 +59,7 @@ end function
 
 subroutine unittest_rungekutta()
 
-  double precision, allocatable y
+  double precision, dimension(1) y
   double precision, size(2) x
   double precision exact
   double precision er
