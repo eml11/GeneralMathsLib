a733b6a446755fded2620e1d64f3f266a6ed784d
eml11 <eml11@ic.ac.uk>
2016-09-01 23:13:29 +0100

    rungekutta_implict written to use rkstep

--- rungekutta_implicit.f90
+++ rungekutta_implicit.f90
@@ -1,60 +1,57 @@
+module mod_rungekutta_implicit
+
+use mod_rungekutta
+
+implicit none
+
+contains
+
+
 subroutine rungekutta_implicit(func,y,x,order)
   ! implicit
   ! anyway to compatmentalise
   ! some of this so that there is
   ! less duplicated code with the 
   ! explicit scheme
+
   interface
     function func(y,x)
-      double precision, allocatable y
+      double precision, dimension(:) :: y
       double precision x
+      double precision, dimension(size(y)) :: func
     end function
   end interface
 
-  double precision, allocatable y
-  double precision, size(2) x
+  double precision, dimension(:) :: y
+  double precision, dimension(2) :: x
   integer order !not sure if there is an efficient formula for this
   
-  double precision, allocatable retar
-  double precision, allocatable k
   double precision deltax
-  
-  deltax = (x(2) - x(1))/100.0
-  
-  do 
-    k = func(y,x(1))
-    retar = retar + k
-  
-    k = func(y + k, x(1) + 0.5D0*deltax)
-    retar = retar + 2D0*k
-  
-    k = func(y + k, x(1) + 0.5D0*deltax)
-    retar = retar + 2D0*k
 
-    k = func(y + k, x(1) + deltax)
-    retar = retar + k
-    retar = retar/6.0D0
+  deltax = (x(2) - x(1))/1000.0
+
+  retar = y
+
+  do
+
+    call rkstep(func,y,x(1),deltax,order)
+
     x(1) = x(1) + deltax
 
-    if x(1).ge.x(2)
-      break
+    ! may need to go at top
+    if (x(1) .ge. x(2)) then
+      exit
     end if
 
   end do
 
 end subroutine
 
-function simple_decay(y,x)
-  double precision, allocatable y
-  double precision x
 
-  return -x
-end function
+subroutine unittest_rungekutta_implicit()
 
-subroutine unittest_rungekutta()
-
-  double precision, allocatable y
-  double precision, size(2) x
+  double precision, dimension(1) :: y
+  double precision, dimension(2) :: x
   double precision exact
   double precision er
 
@@ -67,9 +64,12 @@ subroutine unittest_rungekutta()
 
   exact = DEXP(-x(2))
   er = (exact - y(1))/DABS(exact)
-  
+
   ! eventually print these to a file when unit
   ! testing
-  print(*,*) er
+  print *, er
 
 end subroutine
+
+
+end module 
