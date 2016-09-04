b608277f2cbf5f9eb8e8cbd05dc72edd692d7257
eml11 <eml11@ic.ac.uk>
2016-09-04 15:48:39 +0100

    begun writing the minimization algrithm

--- jacobian_minimize.f90
+++ jacobian_minimize.f90
@@ -1,5 +1,10 @@
 module mod_jacobian_minimize
 
+#define JACOBIAN_RES_TOL 1.0D-6
+
+! not really the best way of doing this
+#define JACOBIAN_DERIVATIVE_STEP = 1.0D-3
+
 contains
 
 subroutine jacobian_minimize(objective,x0,integer_args,double_args,func_arg)
@@ -25,6 +30,32 @@ subroutine jacobian_minimize(objective,x0,integer_args,double_args,func_arg)
 
   integer integer_args(:)
   double precision double_args(:)
+
+  double precision, dimension(size(x0)) :: residual
+
+  integer i
+  double precision, dimension(size(x0),size(x0)) :: jacobian
+
+  do
+
+    residual = objective(x0,integer_args,double_args,func_arg)
+
+    ! Want to compute this here not 
+    ! to do one more step than required.
+    if (residual < JACOBIAN_RES_TOL) then
+      exit
+    end if
+
+    do i = 1, size(x0)
+
+      ! should generalise
+      x0(i) = x0(i) + JACOBIAN_DERIVATIVE_STEP
+      jacobian(:,i) = objective(x0,integer_args,double_args,func_arg)
+      x0(i) = x0(i) - JACOBIAN_DERIVATIVE_STEP
+
+
+    end do
+  end do
 
 end subroutine
 
